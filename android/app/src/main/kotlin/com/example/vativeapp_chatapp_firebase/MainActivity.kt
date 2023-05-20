ed758b9f60386da74be7cce19ce0356d0a4473ac
uroojmajid2000 <urooj.majid2000@gmail.com>
2023-05-21 00:49:07 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/vativeapp_chatapp_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vativeapp_chatapp_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:793665329602:web:a37d3cade207e76430de5b";
+  static String apiKey = "AIzaSyBVCKTFnsNcQf8dwmcyKDvKphIFRKpT3ls";
+  static String messagingSenderId = "793665329602";
+  static String projectId = "chatappflutter-847fb";
+  final primaryColor = const Color(0xFFee7b64);
+}
